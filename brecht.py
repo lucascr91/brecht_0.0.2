e50eeae71f72fa8f1193c3cf783f3561d30e2702
lucascr91 <lucas.ecomg@gmail.com>
2019-12-31 15:10:47 -0200

    Add buttons to third window

--- brecht.py
+++ brecht.py
@@ -1,87 +1,74 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
-
+## This file contains the classes and functions used in the game
 
 import pickle
 import spacy
 from selenium import webdriver
+import tkinter as tk
+from tkinter import ttk
 nlp = spacy.load('de')
 
-
 # ## Opening local dictionary
-
-# In[2]:
-
-
 pickle_in = open('brecht_dict.pkl', 'rb')
 brecht_dict = pickle.load(pickle_in)
 
 
 # ## Opening not found list
-
-# In[4]:
-
-
 pickle_in2 = open('brecht_lista_not_found.pkl', 'rb')
 lista_not_found = pickle.load(pickle_in2)
-
-
-# In[63]:
-
 
 class Text():
     def __init__(self, file):
         self.file = file
+
     @property
     def actual_text(self):
-        f =open(self.file, 'r', encoding = 'utf-8')
+        f = open(self.file, 'r', encoding='utf-8')
         text = f.read()
         f.close()
-        
         return text
-    
+
     @property
     def doc(self):
         parsed_text = nlp(self.actual_text)
-        
         return parsed_text
-        
+
     @property
     def verbs(self):
         verbs = [token.lemma_ for token in self.doc if token.pos_ == "VERB"]
-        
         return verbs
-    
+
     @property
     def nouns(self):
         nouns = [token.lemma_ for token in self.doc if token.pos_ == "NOUN"]
-        
         return nouns
 
-
-# In[42]:
+    @property
+    def adjectives(self):
+        adjectives = [token.lemma_ for token in self.doc if token.pos_ == "ADJ"]
+        return adjectives
 
 
 class Words():
-    def __init__(self, value, g_class, first_meaning = True):
+    def __init__(self, value, g_class, first_meaning=True):
         self.value = value
         self.g_class = g_class
         self.first_meaning = first_meaning
-    #attribute meaning
+    # attribute meaning
     meaning = ''
-    #is the word in my local dictionary?
+    # is the word in my local dictionary?
     @property
     def meaning(self):
         if self.value in brecht_dict.keys():
             return brecht_dict[self.value]
-        #so, get it it online
+        # so, get it it online
         else:
             driver = webdriver.Firefox()
             driver.get('https://de.thefreedictionary.com/{}'.format(self.value))
             word = driver.find_elements_by_xpath('//div[@id="Definition"]')
-            if word!=[]:
+            if word != []:
                 brecht_dict[self.value] = word[0].text
                 if self.first_meaning:
                     return word[0].text[:word[0].text.find('2.')]
@@ -91,16 +78,12 @@ class Words():
                 lista_not_found.append(self.value)
                 return print('I could not find this word')
             driver.close()
-        #method to print value
+        # method to print value
+
         def __str__(self):
             return self.value
 
-
-# In[ ]:
-
-
-if __name__ == '__main__': # para testar no terminal
-    word1 = Words('Beispiel', g_class = 'noun')
+if __name__ == '__main__':  # para testar no terminal
+    word1 = Words('Beispiel', g_class='noun')
     print(word1)
     print(word1.g_class)
-
--- brecht_window3.py
+++ brecht_window3.py
@@ -2,34 +2,36 @@ import tkinter as tk
 from tkinter import ttk
 import brecht as br
 
-def create_instance():
-    global text
-    text = br.Text(user_text.get())
-
-def print_verbs():
-    global verbs
-    verbs = text.verbs
-
-    return text_widget.insert("1.0", str(verbs))
-
-
 root = tk.Tk()
 root.geometry('600x400')
 root.title('Brecht 0.0.2')
 root.resizable(False, False)
 
-# for m in range(3):
-#     root.rowconfigure(m, weight =1)
-
-# root.columnconfigure(0, weight = 4)
-# root.columnconfigure(1, weight = 1)
-# root.columnconfigure(2, weight = 4)
-
-
-
-
-
 user_text = tk.StringVar()
+
+def create_instance():
+    global text
+    text = br.Text(user_text.get())
+
+def print_adjectives():
+    global adjectives
+    adjectives = text.adjectives
+    return text_widget.insert("1.0", str(adjectives))
+
+def print_nouns():
+    global nouns
+    nouns = text.nouns
+    return text_widget.insert("1.0", str(nouns))
+
+def print_verbs():
+    global verbs
+    verbs = text.verbs
+    return text_widget.insert("1.0", str(verbs))
+
+def print_text():
+    global actual_txt
+    actual_txt = text.actual_text
+    return text_widget.insert("1.0", str(actual_txt))
 
 #********************************************FRAMES**********************************
 
@@ -64,13 +66,26 @@ text_widget.grid()
 go_button = ttk.Button(Button_Frame, text = 'Go', command = create_instance)
 go_button.grid(row = 1, column = 0, sticky = 'WE', columnspan = 2)
 
+#BUTTON LIST ADJECTIVES
+adj_button = ttk.Button(Button_Frame, text = 'List Adjectives', command = print_adjectives)
+adj_button.grid(row = 2, column = 0, sticky = 'WE', columnspan = 2)
+
+#BUTTON LIST NOUNS
+nouns_button = ttk.Button(Button_Frame, text = 'List Nouns', command = print_nouns)
+nouns_button.grid(row = 3, column = 0, sticky = 'WE', columnspan = 2)
+
 #BUTTON LIST VERBS
 verbs_button = ttk.Button(Button_Frame, text = 'List Verbs', command = print_verbs)
-verbs_button.grid(row = 2, column = 0, sticky = 'WE', columnspan = 2)
+verbs_button.grid(row = 4, column = 0, sticky = 'WE', columnspan = 2)
+
+#BUTTON FULL TEXT
+text_button = ttk.Button(Button_Frame, text = 'Full Text', command = print_text)
+text_button.grid(row = 5, column = 0, sticky = 'WE', columnspan = 2)
+
 
 #BUTTON QUIT
 quit_button = ttk.Button(Button_Frame, text = 'Quit', command = root.destroy)
-quit_button.grid(row = 3, column = 0, sticky = 'WE', columnspan = 2)
+quit_button.grid(row = 6, column = 0, sticky = 'WE', columnspan = 2)
 
 root.mainloop()
 
