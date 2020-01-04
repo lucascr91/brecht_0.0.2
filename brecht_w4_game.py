142fc67e4b63e39ea171d737cf63a33e2547599c
lucascr91 <lucas.ecomg@gmail.com>
2020-01-04 16:40:07 -0200

    Buttons working in fourth window!

--- brecht_w4_game.py
+++ brecht_w4_game.py
@@ -9,6 +9,16 @@ from time import sleep
 lang = 'DE'
 
 
+text = br.Text('user_text.txt')
+
+adj = text.adjectives
+nouns = text.nouns
+verbs = text.verbs
+
+text_words = adj + nouns + verbs
+
+
+
 # Abre dicionário local
 pickle_in = open('dictionaries_{0}/brecht_dict_{0}.pkl'.format(lang), 'rb')
 brecht_dict = pickle.load(pickle_in)
@@ -31,121 +41,26 @@ def combine_funcs(*funcs):
             f(*args, **kwargs)
     return combined_func
 
-def callback(event):
-    global answer1
-    #print(event)
-    answer1 = event.widget['text']
-
-
-
-def stats():
-    global text_words
-
-    text = br.Text('user_text.txt')
-
-    adj = [token.lemma_ for token in text.doc if token.pos_ == "ADJ"]
-    nouns = [token.lemma_ for token in text.doc if token.pos_ == "NOUN"]
-    verbs = [token.lemma_ for token in text.doc if token.pos_ == "VERB"]
-
-    text_words = adj + nouns + verbs
-
-
-    unique_voc = list(set(brecht_uservocab))
-    unique_txt = list(set(text_words))
-
-    intersection = [i for i in unique_txt if i in unique_voc]
-
-    rz = len(intersection)/len(text_words)
-
-    return text_widget4.insert('1.0', "I identify " + str(len(text_words)) + " unique words. \n"+
-    'You know ' + str(round(rz*100,2)) + '% of them \n')
-
-def words_founded():
+def sort_words():
     global selected_words
-    global current_dict
+    selected_words = random.sample(text_words, 5)
 
-    m=5
-    selected_words = random.sample(text_words, m)
-
-    # Adiciona palavras ao dicionário local
-    for word in selected_words:
-        brecht_dict[word] = br.Words(word).meaning
-
-    # Salvando o dicionário atualizado
-    pickle_out = open('dictionaries_{0}/brecht_dict_{0}'.format(lang), 'wb')
-    pickle.dump(brecht_dict, pickle_out)
-    pickle_out.close()
-
-
-    current_dict = {k: brecht_dict[k] for k in text_words if k in brecht_dict}
-
-    current_dict = {k:v for k, v in current_dict.items() if v!=None}
-    
-    gen = enumerate([i for i in selected_words if i not in current_dict])
-
-    if len(selected_words)>len(current_dict):
-        return text_widget4.insert('4.0', 'I could not find the meaning of the following '+ str(len(selected_words)-len(current_dict)) + ' words.')
-        for index, word in gen:
-            text_widget4.insert('{}.0'.format(str(4+index)), str(index+1)+')'+ word)
-
-        text_widget4.insert('{}.0'.format(str(len(gen))),'So you will play the game with ' + str(len(current_dict)) )
-    else:
-        return text_widget4.insert('4.0',"I found the meaning of all " + str(m)+ " words. Let's begin!")
-
-def start_game():
-    sleep(3)
-    text_widget4.delete('1.0', 'end')
-    while True:
-        brecht_dict2 = current_dict.copy()
-        somenoun = random.sample(brecht_dict2.keys(), 1)[0]
-        while somenoun !='':
-            text_widget4.insert('1.0','Do you know the meaning of the word "{}"?'.format(somenoun))
-            if answer1 == 'No':
-                text_widget4.insert('2.0',brecht_dict2[somenoun])
-                somenoun = random.sample(brecht_dict2.keys(), 1)[0]
-            elif answer1 =='Vocabulary':
-                brecht_uservocab.append(somenoun)
-                del brecht_dict2[somenoun]
-                if brecht_dict2 !={}:
-                    somenoun = random.sample(brecht_dict2.keys(), 1)[0]
-                else:
-                    text_widget4.delete('1.0', 'end')
-                    text_widget4.tag_configure("center", justify='center')
-                    text_widget4.insert('1.0','Congratulations! You know all words from this text!')
-                    somenoun =''
-            elif answer1=='Important':
-                brecht_userimportant.append(somenoun)
-                if brecht_dict2 !={}:
-                    somenoun = random.sample(brecht_dict2.keys(), 1)[0]
-                else:
-                    text_widget4.delete('1.0', 'end')
-                    text_widget4.tag_configure("center", justify='center')
-                    text_widget4.insert('1.0','Congratulations! You know all words from this text!')
-                    somenoun =''                
-                if brecht_dict2 !={}:
-                    somenoun = random.sample(brecht_dict2.keys(), 1)[0]
-                else:
-                    text_widget4.delete('1.0', 'end')
-                    text_widget4.tag_configure("center", justify='center')
-                    text_widget4.insert('1.0','Congratulations! You know all words from this text!')
-                    somenoun =''
-            else:
-                del brecht_dict2[somenoun]
-                if brecht_dict2 !={}:
-                    somenoun = random.sample(brecht_dict2.keys(), 1)[0]
-                else:
-                    text_widget4.delete('1.0', 'end')
-                    text_widget4.tag_configure("center", justify='center')
-                    text_widget4.insert('1.0','Congratulations! You know all words from this text!')
-                    somenoun =''   
+def callback(event):
+    if event.widget['text']=='Yes':
         text_widget4.delete('1.0', 'end')
-        text_widget4.tag_configure("center", justify='center')
-        text_widget4.insert('1.0','Do you want to play this game again?')
-        if answer2=='No':
-            print("Bye!")
-            break
-        else:
-            continue
+        show_word(1)
+    elif event.widget['text']=='No':
+        text_widget4.delete('1.0', 'end')
+        return text_widget4.insert('1.0', 'Here is the meaning: \n'+
+        br.Words(selected_words[0]).meaning)
+    elif event.widget['text']=='Vocabulary':
+        brecht_uservocab.append(selected_words[0])
+        text_widget4.delete('1.0', 'end')
+        return text_widget4.insert('1.0', 'This word is now part of your vocabulary and it will not be show again')
+
+def show_word(index = 0):
+    text_widget4.insert('1.0', 'Do you know the meaning of the word {}?'.format(selected_words[index]))
+    
 
 class Fourth_window(tk.Tk):
     def __init__(self):
@@ -157,22 +72,18 @@ class Fourth_window(tk.Tk):
 class Button_Frame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
-        #BUTTON YES
+
         button1 = ttk.Button(self, text = 'Yes')
-        button1.bind('<Button-1>', callback)
-        button1.pack(side = 'left', padx =5, pady = 5, fill = 'x')
+        container.bind('<Button-1>', callback) 
+        button1.pack(side = 'left')
 
-
-        #BUTTON NO
         button2 = ttk.Button(self, text = 'No')
-        button2.bind('<Button-2>', callback)
-        button2.pack(side = 'left', padx =5, pady = 5, fill = 'x')
+        container.bind('<Button-2>', callback) 
+        button2.pack(side = 'left')
 
-
-        #BUTTON VOCABULARY
         button3 = ttk.Button(self, text = 'Vocabulary')
-        button3.bind('<Button-3>', callback)
-        button3.pack(side = 'left', padx =5, pady = 5, fill = 'x')
+        container.bind('<Button-3>', callback) 
+        button3.pack(side = 'left')
 
 
 class Quit_Frame(ttk.Frame):
@@ -199,10 +110,8 @@ text_w4.pack(side = 'top', fill = 'both', expand = True)
 
 button_w4 = Button_Frame(root)
 button_w4.pack(side = 'bottom')
-# for n in ['1','2', '3']:
-#     button_w4.bind('<Button-{}>'.format(n), function_so)
 
-quit_w4 = Quit_Frame(root, 'Start', combine_funcs(stats, words_founded, start_game))
+quit_w4 = Quit_Frame(root, 'Start', combine_funcs(sort_words, show_word))
 quit_w4.pack(side = 'right')
 
 #WIDGETS
